404dc6015e7914f0ea0aee0fad60c322af1babe2
Abul Hasan <abulhasan.swe@gmail.com>
2022-02-12 21:57:53 -0800

    basics of javascript

---
+++ DOM-js/event-listeners.js
@@ -0,0 +1,14 @@
+const elem11 = document.querySelector(".noclass");
+
+//Adding event listeners
+elem11.addEventListener('mouseover',eve=>{
+    console.log(eve)
+})
+
+//mouserover
+
+//mouseout
+
+//mouseup -- when the mouse button goes up(after the click)
+
+//mousedown -- when the mouse button goes down
---
+++ js/strings.js
@@ -0,0 +1,23 @@
+//template string and strin concatenation
+const name = 'Abul Hasan';
+const years = 25;
+//This is called as string concatenation
+console.log('Hello my name is '+name+'And im ' + years+' old');
+//This is called as template strings
+const greeting = `Hello my name is ${name} and I'm ${years} old`;
+console.log(greeting)
+
+//Properties & methods
+console.log(greeting.length)
+console.log(greeting.toUpperCase())
+console.log(greeting.substring(0,1))
+
+//slice
+
+//splice
+
+//indexOf
+
+//replace
+
+//replaceAll
